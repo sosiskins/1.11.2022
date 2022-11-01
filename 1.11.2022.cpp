a386969172761375bddaac6f056fa299ddb7dacb
sosiskins <117184907+sosiskins@users.noreply.github.com>
2022-11-01 18:37:08 +0300

    Add files via upload

---
+++ 1.11.2022.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+using namespace std;
+/*int main() {
+	int mas[10];
+	mas[0] = 45;
+	for (int i = 0; i <= 10; i++) {
+		cin >> mas[i];
+	}
+	const int size = 10;
+	//1
+	int mas[5];
+	for (int i = 0; i < 5; i++) {
+		cout << mas[i] << ' ';
+	}
+	//2
+	for (int i = 0; i < size; i++) {
+		cout << mas[i] << ' ';
+
+	}
+	//1
+	const int s = 10;
+	int m[s];
+	for (int i = 0; i < s; i++) {
+		cin >> m[i];
+	}
+
+	for (int i = 0; i < s; i++) {
+	}
+	for (int i = 9; i >= 0; i--) {
+		cout << m[i] << ' ';
+	}
+//2
+	const int s = 10;
+	int m[s];
+	for (int i = 0; i < s; i++) {
+		cin >> m[i];
+	}
+	for (int i = 9; i >= 0; i++) {
+		cout << m[i] << ' ';
+	}
+	for (int i = 0; i < s; i++) 
+	if (m [i] % 2 == 0) {
+		int sum = 0;
+		sum += m[i];
+		cout << sum;
+	}*/
+	//3
+	/*const int s = 5;
+	int proiz = 1;
+	int m[s];
+	for (int i = 0; i < s; i++) {
+		cin >> m[i];
+	}
+	for (int i = 0; i < 0; i++) {
+		cout << m[i] << ' ';
+		
+	}
+	for (int i = 0; i < s; i++) {
+		if (i % 2 != 0) {
+			proiz *= m[i];
+			cout << endl;
+		}
+		cout << proiz;*/
+		//4
+/*const int a = 7;
+int m[a];
+for (int i = 0; i < 7; i++) {
+	cin >> m[i];
+	if (m[i] % 3 != 0 && m[i] % 5 == 0) {
+		k++
+	}
+}
+		for (int i = 0; i < 7; i++) {
+			if (m[i] % 3 != 0 && m[i] % 5 == 0) {
+				k++;
+
+				cout << k << ' ';
+			}*/
+			//5
+			const int s = 9;
+			int sum = 0;
+			int ar = 0;
+
+			int m[a];
+			for (int i = 0; i < s; i++) {
+				
+				}
+}
